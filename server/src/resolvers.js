5cc255b44be2aa49277ce27bf9bad523d2ea9770
Teerit Ploensin <teeritploensin@gmail.com>
2023-10-25 00:14:49 +0700

    Add query tracksForHomeFetch

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,10 +3,17 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    tracksForHomeFetch: async () => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+    author: async ({ authorId }) => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/author/${authorId}`);
+      return res.json();
     },
   },
 };
