44ee63f50dae17c2c94efbb262a330a644a8de7c
Teerit Ploensin <teeritploensin@gmail.com>
2023-10-24 23:17:14 +0700

    Install package and implement track-api

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,15 @@
+import { RESTDataSource } from "@apollo/datasource-rest";
+
+class TrackAPI extends RESTDataSource {
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+  getTracksForHome() {
+    return this.get("tracks");
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${authorId}`);
+  }
+}
+
+module.exports = TrackAPI;
